1f17eff083fbe2b8189ba69a853de9dd28ed96cf
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-05-14 14:46:21 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,15 @@ import dummy from '../views/dummy.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
-    {
-      path: '/scenes',
-      name: 'scenes',
-      component: () => import('../views/ScenesView.vue')
-    },
     {
       path: '/',
       name: 'intro',
       component: dummy
+    },
+    {
+      path: '/scenes',
+      name: 'scenes',
+      component: () => import('../views/ScenesView.vue')
     },
     {
       path: '/setup',
